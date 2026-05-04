9c32881ac06638f4e1899a8bf420c93264f020f9
Abi Bhaskara <abibhaskara@192.168.1.10>
2026-05-05 03:08:43 +0800

    fix: delay link navigation to allow click SFX to play first

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -10,18 +10,21 @@ import React from 'react';
 import './LinkButton.css';
 
 const LinkButton = ({ title, url }) => {
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
     const audio = new Audio('/clicksfx.mp3');
     audio.volume = 0.4;
     audio.play().catch(() => {});
+    
+    setTimeout(() => {
+      window.open(url, '_blank', 'noopener,noreferrer');
+    }, 200);
   };
 
   return (
     <a 
       href={url} 
       className="fantasy-button" 
-      target="_blank" 
-      rel="noopener noreferrer"
       onClick={handleClick}
     >
       <div className="corner top-left"></div>
